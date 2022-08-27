e60d8a22f6e758e1ac6a4ecae6630c6a50c1e25d
KateLin1998 <katarina144144@gmail.com>
2022-08-27 23:04:43 +0200

    Shadow change of button when clicked

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function myFunction() {
     var element = document.body;
     element.classList.toggle("dark-mode");
+    document.getElementById("myDIV").style.boxShadow = "3px 3px 2px white";
  }
+
+ 
