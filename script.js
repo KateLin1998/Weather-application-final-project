534e690a13b269034ec1b9d2285f4b99e6160a5e
KateLin1998 <katarina144144@gmail.com>
2022-08-30 22:14:25 +0200

    Added 6 day weather forecast

--- script.js
+++ script.js
@@ -19,29 +19,47 @@ function formatDate(timestamp) {
     return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast() {
+function formatDay(timestamp) {
+let date = new Date(timestamp * 1000);
+let day = date.getDay();
+let days = ["Sun", "Mon", "Tue", "Wedn", "Thu", "Fri", "Sat"];
+
+return days[day];
+}
+
+function displayForecast(response) {
+    let forecast = response.data.daily;
+    
     let forecastElement = document.querySelector("#forecast");
 
+
     let forecastHTML = `<div class="row">`;
-    let days = ["Thu", "Fri", "Sat", "Sun"];
-    days.forEach(function(day) {
-        forecastHTML = forecastHTML + 
-        `
-    
-                <div class="col-2">
-                    <div class="weather-forecast-date">
-                    ${day}</div>
-                <img src="https://ssl.gstatic.com/onebox/weather/48/sunny.png" alt="" width="50" height="50" />
+    forecast.forEach(function(forecastDay, index) {
+        if (index < 6) {
+        forecastHTML = 
+        forecastHTML + 
+        `    
+            <div class="col-2">
+                    <div class="weather-forecast-date">${formatDay(forecastDay.dt)}
+                    </div>
+                <img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" alt="" width="42" />
                 <div class="weather-forecast-temperature">
-                <span class="weather-forecast-temperaute-max">18</span> 
-                <span class="weather-forecast-temperaute-min">12</span>
+                <span class="weather-forecast-temperaute-max"> ${Math.round(forecastDay.temp.max)} /</span> 
+                <span class="weather-forecast-temperaute-min"> ${Math.round(forecastDay.temp.min)}</span>
             </div>
             </div>
         `;
+        }
     })
 
     forecastHTML = forecastHTML + `</div>`;
     forecastElement.innerHTML = forecastHTML; 
+}
+
+function getForecast(coordinates) {
+    let apiKey = "1b6c0398e1da79a12e6750bada098ecc";
+    let apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`; 
+    axios.get(apiURL).then(displayForecast);
 }
 
 function displayTemperature(response) {
@@ -63,6 +81,9 @@ function displayTemperature(response) {
     dateElement.innerHTML = formatDate(response.data.dt * 1000);
     iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
     iconElement.setAttribute("alt", response.data.weather[0].description);
+
+    getForecast(response.data.coord);
+
 }
 
 function search(city) {
@@ -107,4 +128,3 @@ let celsLink = document.querySelector("#cels");
 celsLink.addEventListener("click", displayCelsTemperature);
 
 search("New York");
-displayForecast();
